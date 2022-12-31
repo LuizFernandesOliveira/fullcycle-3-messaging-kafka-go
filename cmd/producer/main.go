044ecfb07960e03e523b9986862e58329b82d721
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2022-12-31 15:45:51 -0300

    feat: add consumer

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -9,21 +9,24 @@ import (
 func main() {
 	deliveryChannel := make(chan kafka.Event)
 	producer := NewKafkaProducer()
-	Publish("Hello World", "test", producer, nil, deliveryChannel)
-	e := <-deliveryChannel
-	msg := e.(*kafka.Message)
-	if msg.TopicPartition.Error != nil {
-		fmt.Println("Erro ao enviar")
-	} else {
-		fmt.Println("Mensagem enviada", msg.TopicPartition)
-	}
-	producer.Flush(1000)
-
+	Publish("Hello World", "test", producer, []byte("transferencia"), deliveryChannel)
+	go DeliveryReport(deliveryChannel)
+	//e := <-deliveryChannel
+	//msg := e.(*kafka.Message)
+	//if msg.TopicPartition.Error != nil {
+	//	fmt.Println("Erro ao enviar")
+	//} else {
+	//	fmt.Println("Mensagem enviada", msg.TopicPartition)
+	//}
+	producer.Flush(10000)
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "fullcycle-3-messaging-kafka-go_kafka_1:9092",
+		"bootstrap.servers":   "fullcycle-3-messaging-kafka-go_kafka_1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",
+		"enable.idempotence":  "true",
 	}
 	p, err := kafka.NewProducer(configMap)
 	if err != nil {
@@ -43,4 +46,17 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 		return err
 	}
 	return nil
+}
+
+func DeliveryReport(deliveryChannel chan kafka.Event) {
+	for e := range deliveryChannel {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v", ev.TopicPartition)
+			} else {
+				fmt.Printf("Delivered message to %v", ev.TopicPartition)
+			}
+		}
+	}
 }
